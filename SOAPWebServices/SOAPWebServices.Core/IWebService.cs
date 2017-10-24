16fb1f45896aeb9d390b50c2304cace43f41f49c
Juan Manuel Batty Linero <jmbl1685@hotmail.com>
2017-10-23 21:12:16 -0500

    Agregando demás archivos y carpetas

---
+++ SOAPWebServices/SOAPWebServices.Core/IWebService.cs
@@ -0,0 +1,36 @@
+﻿using SOAPWebServices.Entities;
+using SOAPWebServices.Entities.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOAPWebServices.Core
+{
+
+    [ServiceContract(Namespace = "soap" )]
+    public interface IWebService
+    {
+
+        [OperationContract(Name = "Post")]
+        Task<ServiceResponse> AddPeople(People people);
+
+        [OperationContract(Name = "Get")]
+        Task<ServiceResponse> GetPeople();
+
+        //[OperationContract(Name = "GetByID")]
+        //Task<ServiceResponse> GetByIDPeople(string UUID);
+
+        //[OperationContract(Name = "Put")]
+        //Task<ServiceResponse> UpdatePeople(string UUID, People people);
+
+        //[OperationContract(Name = "Delete")]
+        //Task<ServiceResponse> RemovePeople(string UUID);
+
+    }
+
+}
---
+++ SOAPWebServices/SOAPWebServices.Core/WebService.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using FireSharp.Response;
+using SOAPWebServices.Entities;
+using SOAPWebServices.Entities.Response;
+
+namespace SOAPWebServices.Core
+{
+    public class Service1 : IWebService
+    {
+        public async Task<ServiceResponse> AddPeople(People people)
+        {
+            var client = FirebaseConnection.Connection.Client();
+            var response = await client.PushAsync("people", people);
+            return new ServiceResponse()
+            {
+                FirebaseID = response.Result.name,
+                Status = response.StatusCode.ToString()
+            };
+        }
+
+        public async Task<ServiceResponse> GetPeople()
+        {
+            var client = FirebaseConnection.Connection.Client();
+            var response = await client.GetAsync("people");
+            return null;
+        }
+
+    }
+}
