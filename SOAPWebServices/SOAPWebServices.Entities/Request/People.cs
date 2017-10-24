a52f9001ee4a603a8716afeaeda3c825e4f4f95d
Juan Manuel Batty Linero <jmbl1685@hotmail.com>
2017-10-23 21:10:43 -0500

    Agregando los modelos [request-response]

---
+++ SOAPWebServices/SOAPWebServices.Entities/Request/People.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace SOAPWebServices.Entities
+{
+    [DataContract]
+    public class People
+    {
+        private Guid uuid { get; set; }
+        private string name { get; set; }
+        private string gender { get; set; }
+        private string image { get; set; }
+
+        [DataMember]
+        public Guid UUID
+        {
+            get => uuid;
+            set => uuid = value;
+        }
+
+        [DataMember]
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        [DataMember]
+        public string Gender
+        {
+            get => gender;
+            set => gender = value;
+        }
+
+        [DataMember]
+        public string Image
+        {
+            get => image;
+            set => image = value;
+        }
+
+    }
+
+}
---
+++ SOAPWebServices/SOAPWebServices.Entities/Response/ServiceResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace SOAPWebServices.Entities.Response
+{
+    [DataContract]
+    public class ServiceResponse
+    {
+        private string firebaseid { get; set; }
+        private string status { get; set; }
+
+        [DataMember]
+        public string FirebaseID
+        {
+            get => firebaseid;
+            set => firebaseid = value;
+        }
+
+        [DataMember]
+        public string Status
+        {
+            get => status;
+            set => status = value;
+        }
+
+    }
+}
