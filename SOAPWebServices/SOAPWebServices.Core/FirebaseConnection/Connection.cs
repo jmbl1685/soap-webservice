edbd5f27e78099aea1c71a9d2630aecdf7a7e209
Juan Manuel Batty Linero <jmbl1685@hotmail.com>
2017-10-23 21:11:51 -0500

    Agregando las db al web service [firebase]

---
+++ SOAPWebServices/SOAPWebServices.Core/FirebaseConnection/Connection.cs
@@ -0,0 +1,24 @@
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace SOAPWebServices.Core.FirebaseConnection
+{
+    public static class Connection
+    {
+        private static IFirebaseConfig Config() => new FirebaseConfig
+        {
+            AuthSecret = ConfigurationManager.AppSettings["firebase_authsecret"].ToString(),
+            BasePath = ConfigurationManager.AppSettings["firebase_basepath"].ToString()
+        };
+
+        public static IFirebaseClient Client() => new FirebaseClient(Config());
+
+    }
+
+}
