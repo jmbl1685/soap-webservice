983cba2cba333d55add3a3c350f4f6fc661a4003
Juan Manuel Batty Linero <jmbl1685@hotmail.com>
2017-10-27 22:04:09 -0500

    Modificación PeopleController [API]

--- WebSite/WebSite.WebApi/Controllers/PeopleController.cs
+++ WebSite/WebSite.WebApi/Controllers/PeopleController.cs
@@ -4,16 +4,53 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebSite.Service;
+using WebSite.Service.WebService.SOAP_Firebase;
 
 namespace WebSite.WebApi.Controllers
 {
     [RoutePrefix("api")]
     public class PeopleController : ApiController
     {
-        [HttpGet, Route("people")]
-        public HttpResponseMessage Get()
+        SoapService webServicesCall;
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PeopleController()
         {
-            return Request.CreateResponse(HttpStatusCode.OK, new { message = "OK" });
+            webServicesCall = new SoapService();
         }
+
+        [HttpPost, Route("people")]
+        public HttpResponseMessage POST([FromBody] People people )
+        {
+            var response = webServicesCall.AddPeople(people); 
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
+
+        [HttpGet, Route("people"), Route("people/{key}")]
+        public HttpResponseMessage GET(string key = null)
+        {
+            var response = (key is null) 
+                ? webServicesCall.GetPeople() : webServicesCall.GetByIDPeople(key);
+
+            return Request.CreateResponse(HttpStatusCode.OK, new { response });
+        }
+
+        [HttpPut, Route("people/{key}")]
+        public HttpResponseMessage PUT(string key, [FromBody] People people)
+        {
+            var response = webServicesCall.UpdatePeople(key,people);
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
+
+        [HttpDelete, Route("people/{key}")]
+        public HttpResponseMessage DELETE(string key)
+        {
+            var response = webServicesCall.RemovePeople(key);
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
+
+
     }
 }
